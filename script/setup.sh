480b5471aac25254076d5f9ef7a033ad3c7b1a73
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2019-12-20 20:39:55 +0100

    Adding common

--- script/setup.sh
+++ script/setup.sh
@@ -3,8 +3,8 @@ rm -rf src/tinyos-main
 git clone https://github.com/tinyos/tinyos-main src/tinyos-main
 
 
-chmod +x ./patchs/archlinux_build_patch_tinyos_main.sh
-./patchs/archlinux_build_patch_tinyos_main.sh
+chmod +x ./common/patchs/archlinux_build_patch_tinyos_main.sh
+./common/patchs/archlinux_build_patch_tinyos_main.sh
 
 cd src/tinyos-main/tools
 
