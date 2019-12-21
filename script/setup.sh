3fce58735c12e2efe211ea5eb7e5ff3fc6f538ca
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2019-12-21 14:05:17 +0100

    Updating setup and common

--- script/setup.sh
+++ script/setup.sh
@@ -6,12 +6,24 @@ git clone https://github.com/tinyos/tinyos-main src/tinyos-main
 chmod +x ./common/patchs/archlinux_build_patch_tinyos_main.sh
 ./common/patchs/archlinux_build_patch_tinyos_main.sh
 
+
+
+chmod +x ./common/patchs/documentation_patch.sh
+./common/patchs/documentation_patch.sh
+
 cd src/tinyos-main/tools
 
 
+echo "======> Building tools"
 ./Bootstrap
 ./configure
 make
+
+
+
+echo "======> Building doc"
+cd ../doc
+make all
 
 
 
